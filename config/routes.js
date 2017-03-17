4e6bc25a29c4d9142875a5f449222e6fa9d5a2be
John Evans <johne103@hotmail.com>
2017-03-17 19:15:12 +0000

    Add routes and top links

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => res.render('index'));
-
+router.get('/contacts', (req, res) => res.render('contacts'));
+router.get('/whoweare', (req, res) => res.render('whoweare'));
+router.get('/mission', (req, res) => res.render('mission'));
+router.get('/events', (req, res) => res.render('events'));
 
 module.exports = router;
